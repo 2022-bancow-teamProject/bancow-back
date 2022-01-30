fd784991424119ef97fe302d4360ac3f1d1e23a5
lowgiant <lowgiant@gmail.com>
2022-01-30 16:26:09 +0900

    conf: SpringBoot init

---
+++ spring/src/main/java/com/bancow/bancowback/BancowbackApplication.java
@@ -0,0 +1,13 @@
+package com.bancow.bancowback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancowbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancowbackApplication.class, args);
+    }
+
+}
---
+++ spring/src/test/java/com/bancow/bancowback/BancowbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancow.bancowback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancowbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
