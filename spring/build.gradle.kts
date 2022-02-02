fa5630261953182b5ecb9bc6c77a078ec0a10c51
kho903 <gmldnr2222@naver.com>
2022-02-02 17:42:30 +0900

    Conf: build.gradle, application.yml, security initial setting

---
+++ spring/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+    id("java")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+lateinit var asciidoctorExt: Configuration
+val snippetsDir = file("build/generated-snippets")
+
+asciidoctorj {
+    asciidoctorExt = configurations.create("asciidoctorExt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+ext {
+    set("snippetsDir", file("build/generated-snippets"))
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("com.auth0:java-jwt:3.18.3")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.security:spring-security-test:5.6.1")
+
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.bootJar {
+    dependsOn(tasks.asciidoctor)
+    from("${tasks.asciidoctor.get().outputDir}") {
+        into("static/docs")
+    }
+}
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+    dependsOn(tasks.test)
+    val snippets = file("build/generated-snippets")
+    attributes["snippets"] = snippets
+    inputs.dir(snippets)
+}
