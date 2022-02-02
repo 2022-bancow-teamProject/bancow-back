43b5e65a82693939965762b44ac571011b2515b2
kho903 <gmldnr2222@naver.com>
2022-02-03 01:46:25 +0900

    feat: Manager Status PENDING_SUPER to ADMIN [#10]

--- spring/src/main/java/com/bancow/bancowback/manager/controller/ManagerController.java
+++ spring/src/main/java/com/bancow/bancowback/manager/controller/ManagerController.java
@@ -5,10 +5,13 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bancow.bancowback.common.dto.Response;
@@ -41,6 +44,12 @@ public class ManagerController {
 	@GetMapping("/authentication/{token}")
 	public ResponseEntity<?> authentication(@PathVariable String token) {
 		ServiceResult result = managerService.authentication(token);
+		return ResponseEntity.ok().body(new Response<>(result, HttpStatus.OK));
+	}
+
+	@PatchMapping("/status-to-admin")
+	public ResponseEntity<?> statusToAdmin(@RequestHeader("TOKEN") String token, @RequestParam Long id) {
+		ServiceResult result = managerService.statusToAdmin(token, id);
 		return ResponseEntity.ok().body(new Response<>(result, HttpStatus.OK));
 	}
 }
