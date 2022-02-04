66948fd22a0a6d7ce60cee0aba97f31c4f0e734b
kho903 <gmldnr2222@naver.com>
2022-02-04 14:58:27 +0900

    test: document login test using Spring REST Docs [#36]

--- spring/src/test/java/com/bancow/bancowback/domain/manager/controller/ManagerControllerTest.java
+++ spring/src/test/java/com/bancow/bancowback/domain/manager/controller/ManagerControllerTest.java
@@ -1,13 +1,17 @@
 package com.bancow.bancowback.domain.manager.controller;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import com.bancow.bancowback.TestSupport;
+import com.bancow.bancowback.domain.manager.dto.ManagerRegisterDto;
+import com.bancow.bancowback.domain.manager.service.ManagerService;
 
 class ManagerControllerTest extends TestSupport {
 
@@ -45,4 +49,33 @@ class ManagerControllerTest extends TestSupport {
 		;
 	}
 
+	@Test
+	void login() throws Exception {
+		mockMvc.perform(
+			post("/api/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(
+					"{\n"
+						+ "  \"email\": \"smtptestkk@gmail.com\",\n"
+						+ "  \"password\": \"1111\"\n"
+						+ "}"
+				)
+		)
+			.andExpect(status().isOk())
+			.andDo(
+				restDocs.document(
+					requestFields(
+						fieldWithPath("email").description("이메일"),
+						fieldWithPath("password").description("비밀번호")
+					),
+					responseFields(
+						fieldWithPath("data").description("결과 데이터"),
+						fieldWithPath("data.result").description("로그인 성공 여부"),
+						fieldWithPath("data.message").description("response 메시지"),
+						fieldWithPath("status").description("HTTP Status")
+					)
+				)
+			)
+		;
+	}
 }
