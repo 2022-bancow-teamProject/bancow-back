83d6050bebd932e908a32b35e3edd973e73b543e
JungKyungMin <46745294+JungKyungMin@users.noreply.github.com>
2022-02-07 21:44:53 +0900

    add NoticeRepository entity #70

---
+++ spring/src/main/java/com/bancow/bancowback/domain/main/notice/repository/NoticeRepository.java
@@ -0,0 +1,15 @@
+package com.bancow.bancowback.domain.main.notice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.bancow.bancowback.domain.main.notice.entity.Notice;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+	Optional<List<Notice>> findByIdIn(List<Long> idList);
+
+}
