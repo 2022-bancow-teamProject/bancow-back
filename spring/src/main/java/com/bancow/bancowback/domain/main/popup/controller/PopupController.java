732c74ff2a551576c373c2109675d6bc2aa22cc9
lowgiant <lowgiant@gmail.com>
2022-02-08 03:08:56 +0900

    feat: Add PopupDetail PopupController [#78]

--- spring/src/main/java/com/bancow/bancowback/domain/main/popup/controller/PopupController.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/popup/controller/PopupController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,12 +33,19 @@ public class PopupController {
 	private final TokenService tokenService;
 
 	@PostMapping("/add")
-	public Response<?> addPopup(@RequestHeader("TOKEN") String token, @Valid @RequestPart("popup_request") PopupAddRequestDto dto,
+	public Response<?> addPopup(@RequestHeader("TOKEN") String token,
+		@Valid @RequestPart("popup_request") PopupAddRequestDto dto,
 		@RequestPart(value = "popup_image", required = false) MultipartFile popupImage) throws IOException {
 		tokenService.validTokenAuthority(token);
 		String ImageUploadPath = ncpService.objectUpload("popup", popupImage);
 		PopupInfo popupInfo = new PopupInfo<PopupAddRequestDto>(tokenService.getManager(token), dto, ImageUploadPath);
 		return new Response<>(popupService.addPopup(popupInfo), HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public Response<?> getPopupDetail(@RequestHeader("TOKEN") String token, @PathVariable Long id) {
+		tokenService.validTokenAuthority(token);
+		return new Response<>(popupService.getPopupDetail(id), HttpStatus.OK);
 	}
 
 }
