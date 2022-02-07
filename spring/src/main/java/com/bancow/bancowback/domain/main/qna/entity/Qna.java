4a6209c1b7bc197cfaa0a67b4f0c747113189187
kho903 <gmldnr2222@naver.com>
2022-02-07 21:47:57 +0900

    feat: add Qna Entity, Qna Category Enum [#73]

---
+++ spring/src/main/java/com/bancow/bancowback/domain/main/qna/entity/Qna.java
@@ -0,0 +1,52 @@
+package com.bancow.bancowback.domain.main.qna.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Qna {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private QnaCategory category;
+
+	@Column(name = "phone_number", nullable = false)
+	private String phoneNumber;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String message;
+
+	@Column(nullable = false)
+	private boolean checked;
+
+	@Column(name = "create_date", nullable = false)
+	private LocalDateTime createDate;
+}
