ae4f781b53a5f0c59d2935c84a8b698555fc518a
JungKyungMin <46745294+JungKyungMin@users.noreply.github.com>
2022-02-07 21:40:33 +0900

    add NoticeDeleteListDto entity #70

---
+++ spring/src/main/java/com/bancow/bancowback/domain/main/notice/dto/NoticeDeleteListDto.java
@@ -0,0 +1,13 @@
+package com.bancow.bancowback.domain.main.notice.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoticeDeleteListDto {
+
+	private List<Long> idList;
+}
