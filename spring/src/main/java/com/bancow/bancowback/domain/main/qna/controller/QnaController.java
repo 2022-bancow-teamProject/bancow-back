4a7ecebdc070d702ea4492eaf63deee2baf91873
kho903 <gmldnr2222@naver.com>
2022-02-08 14:43:55 +0900

    feat: add deletQna in QnaController [#89]

--- spring/src/main/java/com/bancow/bancowback/domain/main/qna/controller/QnaController.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/qna/controller/QnaController.java
@@ -2,14 +2,17 @@ package com.bancow.bancowback.domain.main.qna.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bancow.bancowback.domain.common.dto.Response;
+import com.bancow.bancowback.domain.common.dto.ServiceResult;
 import com.bancow.bancowback.domain.main.qna.entity.Qna;
 import com.bancow.bancowback.domain.main.qna.service.QnaService;
 
@@ -33,4 +36,11 @@ public class QnaController {
 		Page<Qna> allQna = qnaService.getAllQna(page, token);
 		return new Response<>(allQna, HttpStatus.OK);
 	}
+
+	@DeleteMapping("/{id}")
+	public Response<?> deleteQna(@PathVariable Long id, @RequestHeader("TOKEN") String token) {
+		ServiceResult result = qnaService.deleteQna(token, id);
+		return new Response<>(result, HttpStatus.OK);
+	}
+
 }
