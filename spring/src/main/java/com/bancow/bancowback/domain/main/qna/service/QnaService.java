4bde616c1bd19d7568545c777172ee9d9a055283
kho903 <gmldnr2222@naver.com>
2022-02-08 14:43:42 +0900

    feat: add deletQna in QnaService [#89]

--- spring/src/main/java/com/bancow/bancowback/domain/main/qna/service/QnaService.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/qna/service/QnaService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.bancow.bancowback.domain.common.dto.ServiceResult;
 import com.bancow.bancowback.domain.common.exception.QnaException;
 import com.bancow.bancowback.domain.common.util.token.service.TokenService;
 import com.bancow.bancowback.domain.main.qna.entity.Qna;
@@ -36,5 +37,14 @@ public class QnaService {
 		return qnaRepository.findAll(
 			PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "id"))
 		);
+	}
+
+	public ServiceResult deleteQna(String token, Long id) {
+		tokenService.validTokenAuthority(token);
+		Qna qna = qnaRepository.findById(id)
+			.orElseThrow(() -> new QnaException(NOT_Found_QNA, "해당 ID의 QnA를 찾을 수 없습니다."));
+
+		qnaRepository.delete(qna);
+		return ServiceResult.success("QnA를 성공적으로 삭제하였습니다.");
 	}
 }
