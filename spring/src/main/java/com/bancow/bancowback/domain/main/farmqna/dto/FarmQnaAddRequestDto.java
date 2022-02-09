67b6f8aa7d09dc3df16d8fcf599cc850b92f70ed
kho903 <gmldnr2222@naver.com>
2022-02-09 16:13:21 +0900

    feat: add FarmQnaAddRequestDto for addFarmQna [#132]

---
+++ spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/dto/FarmQnaAddRequestDto.java
@@ -0,0 +1,37 @@
+package com.bancow.bancowback.domain.main.farmqna.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FarmQnaAddRequestDto {
+
+	@NotBlank
+	private String name;
+
+	@NotBlank
+	private String phoneNumber;
+
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private String farmName;
+
+	@NotBlank
+	private String farmAddress;
+
+	@NotNull
+	private Integer cowNum;
+
+	@NotBlank
+	private String feedName;
+}
