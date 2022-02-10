1bdedd62d29c6c6fe8e734474ec7da5ee176d872
lowgiant <lowgiant@gmail.com>
2022-02-10 12:55:37 +0900

    feat: findByStatus to PopupRepository [#148]

--- spring/src/main/java/com/bancow/bancowback/domain/main/popup/repository/PopupRepository.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/popup/repository/PopupRepository.java
@@ -1,7 +1,6 @@
 package com.bancow.bancowback.domain.main.popup.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +8,6 @@ import com.bancow.bancowback.domain.main.popup.entity.Popup;
 
 public interface PopupRepository extends JpaRepository<Popup, Long> {
 	List<Popup> findByIdIn(List<Long> idList);
+
+	Popup findByStatus(boolean b);
 }
