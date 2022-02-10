71b72fe3d9e37be6ec7761b97025f181da8bdd78
kho903 <gmldnr2222@naver.com>
2022-02-10 13:31:35 +0900

    feat: getFarmQnaPaging in FarmQnaController [#152]

--- spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/controller/FarmQnaController.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/controller/FarmQnaController.java
@@ -2,6 +2,7 @@ package com.bancow.bancowback.domain.main.farmqna.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bancow.bancowback.domain.common.dto.Response;
@@ -36,5 +38,11 @@ public class FarmQnaController {
 	public Response<FarmQna> getFarmQna(@PathVariable Long id, @RequestHeader("TOKEN") String token) {
 		FarmQna farmQna = farmQnaService.getFarmQna(token, id);
 		return new Response<>(farmQna, HttpStatus.OK);
+	}
+
+	@GetMapping
+	public Response<Page<FarmQna>> getFarmQnaPaging(@RequestParam int page, @RequestHeader("TOKEN") String token) {
+		Page<FarmQna> pagingFarmQna = farmQnaService.getFarmQnaPaging(page, token);
+		return new Response<>(pagingFarmQna, HttpStatus.OK);
 	}
 }
