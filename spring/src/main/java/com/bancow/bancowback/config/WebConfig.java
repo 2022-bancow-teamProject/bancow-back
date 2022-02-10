e959b4345e828636839a909c48c92b067718b4d1
lowgiant <lowgiant@gmail.com>
2022-02-10 10:54:45 +0900

    fix: Add CORS Detail [#138]

--- spring/src/main/java/com/bancow/bancowback/config/WebConfig.java
+++ spring/src/main/java/com/bancow/bancowback/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.bancow.bancowback.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	private final long MAX_AGE_SECS = 3600;
+
+	@Value("${url.main}")
+	private String main;
+
+	@Value("${url.admin}")
+	private String admin;
+
+	@Value("${url.client}")
+	private String client;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8080");
+			.allowedOrigins(client, admin, main)
+			.allowedMethods("GET","POST", "PUT","DELETE","PATCH")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
 	}
 }
