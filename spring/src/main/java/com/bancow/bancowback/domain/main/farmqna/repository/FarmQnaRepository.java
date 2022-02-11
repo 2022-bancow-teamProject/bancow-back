ad2f8a18204f3191695fc27755c26b9eb61067bc
kho903 <gmldnr2222@naver.com>
2022-02-12 00:56:44 +0900

    feat: add count @Query in FarmQnaRepository, QnaRepository [#209]

--- spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/repository/FarmQnaRepository.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/repository/FarmQnaRepository.java
@@ -3,6 +3,7 @@ package com.bancow.bancowback.domain.main.farmqna.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bancow.bancowback.domain.main.farmqna.entity.FarmQna;
@@ -11,4 +12,10 @@ import com.bancow.bancowback.domain.main.farmqna.entity.FarmQna;
 public interface FarmQnaRepository extends JpaRepository<FarmQna, Long> {
 
 	List<FarmQna> findByIdIn(List<Long> idList);
+
+	@Query("select count(f) from FarmQna f")
+	Integer countFarmQna();
+
+	@Query("select count(f) from FarmQna f where f.checked = false")
+	Integer uncheckedFarmQna();
 }
--- spring/src/main/java/com/bancow/bancowback/domain/main/qna/repository/QnaRepository.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/qna/repository/QnaRepository.java
@@ -1,10 +1,16 @@
 package com.bancow.bancowback.domain.main.qna.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bancow.bancowback.domain.main.qna.entity.Qna;
 
 @Repository
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+	@Query("select count(q) from Qna q")
+	Integer countQna();
+
+	@Query("select count(q) from Qna q where q.checked = false")
+	Integer uncheckedQna();
 }
