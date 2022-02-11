bc4b99a62e57a67f5af676653e7d8c7ceded9719
kho903 <gmldnr2222@naver.com>
2022-02-11 14:44:03 +0900

    feat: add FarmQnaReplyRepository [#177]

---
+++ spring/src/main/java/com/bancow/bancowback/domain/main/farmqna/repository/FarmQnaReplyRepository.java
@@ -0,0 +1,11 @@
+package com.bancow.bancowback.domain.main.farmqna.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bancow.bancowback.domain.main.farmqna.entity.FarmQnaReply;
+
+@Repository
+public interface FarmQnaReplyRepository extends JpaRepository<FarmQnaReply, Long> {
+
+}
