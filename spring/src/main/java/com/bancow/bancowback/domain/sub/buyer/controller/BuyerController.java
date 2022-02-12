3bbc2356d20ab9e04156a11c030ec21f50bd79f5
lowgiant <lowgiant@gmail.com>
2022-02-12 23:08:14 +0900

    feat: Edit Buyer [#237]

--- spring/src/main/java/com/bancow/bancowback/domain/sub/buyer/controller/BuyerController.java
+++ spring/src/main/java/com/bancow/bancowback/domain/sub/buyer/controller/BuyerController.java
@@ -1,12 +1,19 @@
 package com.bancow.bancowback.domain.sub.buyer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bancow.bancowback.domain.common.dto.Response;
 
+import com.bancow.bancowback.domain.common.util.token.service.TokenService;
+import com.bancow.bancowback.domain.sub.buyer.dto.BuyerUpdateRequestDto;
 import com.bancow.bancowback.domain.sub.buyer.service.BuyerService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +23,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/buyer")
 public class BuyerController {
 	private final BuyerService buyerService;
+	private final TokenService tokenService;
 
 	@GetMapping("/distribute")
 	public Response<?> getBuyerDistribute() {
 		return new Response<>(buyerService.getBuyerDistribute(), HttpStatus.OK);
 	}
 
+	@PatchMapping("/edit")
+	public Response<?> editBuyer(@RequestHeader("TOKEN") final String token,
+		@Valid @RequestBody BuyerUpdateRequestDto buyerUpdateRequestDto) {
+		tokenService.validTokenAuthority(token);
+		return new Response<>(buyerService.editBuyer(buyerUpdateRequestDto), HttpStatus.OK);
+	}
 }
