fb410bce69d3881b1a414b8edda8eda6007738df
lowgiant <lowgiant@gmail.com>
2022-02-12 23:13:04 +0900

    test: Edit Buyer [#239]

--- spring/src/test/java/com/bancow/bancowback/domain/sub/buyer/controller/BuyerControllerTest.java
+++ spring/src/test/java/com/bancow/bancowback/domain/sub/buyer/controller/BuyerControllerTest.java
@@ -1,11 +1,13 @@
 package com.bancow.bancowback.domain.sub.buyer.controller;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bancow.bancowback.TestSupport;
@@ -41,5 +43,37 @@ class BuyerControllerTest extends TestSupport {
 			)
 		;
 	}
+
+	@Test
+	@Transactional
+	void editBuyer() throws Exception {
+		Manager adminManager = adminManagerLogin();
+		Token tokenAdmin = tokenRepository.findByManager(adminManager).get();
+
+		mockMvc.perform(
+				patch("/api/buyer/edit")
+					.contentType(MediaType.APPLICATION_JSON)
+					.header("TOKEN", tokenAdmin.getToken())
+					.content(readJson("/json/buyer/update.json"))
+			)
+
+			.andExpect(status().isOk())
+			.andDo(restDocs.document(
+				requestHeaders(
+					headerWithName("TOKEN").description("해당 로그인 유저의 토큰값")
+				),
+				requestFields(
+					fieldWithPath("id").description("번호"),
+					fieldWithPath("status").description("공개여부 값")
+				),
+				responseFields(
+					fieldWithPath("data").description("결과 데이터"),
+					fieldWithPath("data.result").description("인증 성공 여부"),
+					fieldWithPath("data.message").description("response 메시지"),
+					fieldWithPath("status").description("HTTP Status")
+				)
+			));
+	}
+
 
 }
