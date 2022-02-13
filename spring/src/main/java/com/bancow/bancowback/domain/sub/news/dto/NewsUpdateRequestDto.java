c71ee227c1023c17c2917b76087385c86484f658
kho903 <gmldnr2222@naver.com>
2022-02-13 13:32:01 +0900

    feat: add news dto [#275]

---
+++ spring/src/main/java/com/bancow/bancowback/domain/sub/news/dto/NewsUpdateRequestDto.java
@@ -0,0 +1,27 @@
+package com.bancow.bancowback.domain.sub.news.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsUpdateRequestDto {
+
+	@NotEmpty
+	private Long id;
+
+	@NotEmpty
+	private String title;
+
+	@NotEmpty
+	private String media;
+
+	@NotEmpty
+	private String url;
+}
