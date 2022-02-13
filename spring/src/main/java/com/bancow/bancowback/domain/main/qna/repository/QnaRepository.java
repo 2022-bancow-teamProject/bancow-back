66291e7cae299d3a7f7a59300866b61b06e09920
kho903 <gmldnr2222@naver.com>
2022-02-13 23:12:50 +0900

    add findByIdIn in QnaRepository [#337]

--- spring/src/main/java/com/bancow/bancowback/domain/main/qna/repository/QnaRepository.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/qna/repository/QnaRepository.java
@@ -21,4 +21,6 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
 		"select concat(year(q.create_date) , '-', lpad(month(q.create_date), 2, '0')) year_month, count(q.create_date) month_count "
 			+ "from qna q where year(q.create_date) = :#{#year} group by year_month")
 	List<Map<String, Object>> countMonth(int year);
+
+	List<Qna> findByIdIn(List<Long> id);
 }
