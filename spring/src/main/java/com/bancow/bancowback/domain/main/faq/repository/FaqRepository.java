7aaf4f508203558d8f1cf5e7c239039189ae620f
kho903 <gmldnr2222@naver.com>
2022-02-13 21:59:25 +0900

    feat: add findByTitleContaining in FaqRepository [#327]

--- spring/src/main/java/com/bancow/bancowback/domain/main/faq/repository/FaqRepository.java
+++ spring/src/main/java/com/bancow/bancowback/domain/main/faq/repository/FaqRepository.java
@@ -3,7 +3,6 @@ package com.bancow.bancowback.domain.main.faq.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.bancow.bancowback.domain.main.faq.entity.Faq;
 
@@ -11,4 +10,5 @@ public interface FaqRepository extends JpaRepository<Faq, Long> {
 
 	List<Faq> findByIdIn(List<Long> idList);
 
+	List<Faq> findByTitleContaining(String word);
 }
