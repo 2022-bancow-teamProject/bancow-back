8d7f7ec5cc1afdd2a7dba8b7568727694764ff73
kho903 <gmldnr2222@naver.com>
2022-02-13 17:15:16 +0900

    feat: add toKoreanCowResponse in ChartMapper [#291]

--- spring/src/main/java/com/bancow/bancowback/domain/sub/chart/mapper/ChartMapper.java
+++ spring/src/main/java/com/bancow/bancowback/domain/sub/chart/mapper/ChartMapper.java
@@ -1,14 +1,22 @@
 package com.bancow.bancowback.domain.sub.chart.mapper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import com.bancow.bancowback.domain.sub.chart.dto.KoreanCowResponseDto;
 import com.bancow.bancowback.domain.sub.chart.dto.QnaNumResponseDto;
+import com.bancow.bancowback.domain.sub.chart.entity.KoreanCow;
+import com.bancow.bancowback.domain.sub.chart.entity.KoreanCowCategory;
 
 @Mapper(componentModel = "spring")
-public interface QnaChartMapper {
+public interface ChartMapper {
 
-	QnaChartMapper INSTANCE = Mappers.getMapper(QnaChartMapper.class);
+	ChartMapper INSTANCE = Mappers.getMapper(ChartMapper.class);
 
 	default public QnaNumResponseDto toResponse(
 		Integer qnaNum,
@@ -21,6 +29,19 @@ public interface QnaChartMapper {
 			.uncheckedQna(uncheckedQna)
 			.farmQnaNum(farmQnaNum)
 			.uncheckedFarmQna(uncheckedFarmQna)
+			.build();
+	}
+
+	default public KoreanCowResponseDto toKoreanCowResponse(KoreanCowCategory category, List<KoreanCow> cowList) {
+		List<Map<Integer, Long>> infoList = new ArrayList<>();
+		Map<Integer, Long> info = new HashMap<>();
+		cowList.forEach(e ->
+			info.put(e.getInfoYear(), e.getInfo())
+		);
+		infoList.add(info);
+		return KoreanCowResponseDto.builder()
+			.category(category)
+			.info(infoList)
 			.build();
 	}
 }
